b2e24ad3d25553cfc3171d889fc9042abf42bfd8
AyaAbdElmoneim158 <ayaabdelmon@gmail.com>
2023-10-07 23:00:48 +0300

    Add Model

--- todo_app/lib/features/auth/model/user_model.dart
+++ todo_app/lib/features/auth/model/user_model.dart
@@ -1,7 +1,7 @@
 class UserModel {
-  final String name;
-  final String email;
-  final String image;
+  final String? name;
+  final String? email;
+  final String? image;
 
-  UserModel({this.name = '', this.email='', this.image=''});
+  UserModel({this.name , this.email, this.image});
 }
